2d93ed9290749348e4908455ceb2ded995cab89f
andyjohn23 <andrewnick906@gmail.com>
2024-12-10 16:36:16 +0300

    add content

---
+++ arrays.js
@@ -0,0 +1,1 @@
+let recipes = ["chicken", "rice", "minced meat", "pilau", "fish"];
---
+++ objects.js
@@ -0,0 +1,9 @@
+let recipes = [
+  {
+    id: 1,
+    recipeName: "fish",
+    recipeImage: "<image_link>",
+    recipeIngredients: ["fish", "onions", "tomatoes"],
+    recipePreparationInstruction: ["step 1", "step 2"],
+  },
+];
